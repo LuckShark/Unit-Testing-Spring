0c3d20e3c588752e00d6c0ad0a030d0e3c5eaf4a
lucasaraujoalmeida <87248601+LuckShark@users.noreply.github.com>
2023-12-07 22:00:01 -0300

    testing category class

--- src/main/java/com/luckshark/demo/resources/CategoriaController.java
+++ src/main/java/com/luckshark/demo/resources/CategoriaController.java
@@ -1,14 +1,26 @@
 package com.luckshark.demo.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.luckshark.demo.entities.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaController {
 	@RequestMapping(method = RequestMethod.GET)
-	public String list() {
-		return "resutado-do-get";
+	public List<Categoria> list() {
+		Categoria cat1 = new Categoria(1,"Informática");
+		Categoria cat2 = new Categoria(2,"Escritória");
+
+		List<Categoria> categorias = new ArrayList<>();
+		categorias.add(cat1);
+		categorias.add(cat2);
+		
+		return categorias;
 	}
 }
