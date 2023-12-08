d7ba8fd58c6e62b1b5576ffdb49e8c911c99e6cf
lucasaraujoalmeida <87248601+LuckShark@users.noreply.github.com>
2023-12-08 12:37:58 -0300

    Pedido class added

---
+++ src/main/java/com/luckshark/demo/entities/Pedido.java
@@ -0,0 +1,63 @@
+package com.luckshark.demo.entities;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+@Entity
+public class Pedido implements Serializable{
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer id;
+	private Date instante;
+	
+	public Pedido() {
+		
+	}
+	
+	public Pedido (Integer id, Date instante) {
+		this.id = id;
+		this.instante = instante;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Date getInstante() {
+		return instante;
+	}
+
+	public void setInstante(Date instante) {
+		this.instante = instante;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pedido other = (Pedido) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+
+}
